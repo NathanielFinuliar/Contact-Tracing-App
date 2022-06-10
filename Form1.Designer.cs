af7920f61341727d82e2c5914f430d6d8bc6989c
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2022-06-10 20:43:41 +0800

    Did some changes in formatting

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,11 +36,17 @@
             this.SFTxtBox = new System.Windows.Forms.TextBox();
             this.NameLabl = new System.Windows.Forms.Label();
             this.CTLabl = new System.Windows.Forms.Label();
-            this.DtPckr = new System.Windows.Forms.DateTimePicker();
             this.FNLabl = new System.Windows.Forms.Label();
             this.MNLabl = new System.Windows.Forms.Label();
             this.LNLabl = new System.Windows.Forms.Label();
             this.SufLabl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.DtVstLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Sbmt
@@ -57,7 +63,7 @@
             // FNTxtBox
             // 
             this.FNTxtBox.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.FNTxtBox.Location = new System.Drawing.Point(33, 118);
+            this.FNTxtBox.Location = new System.Drawing.Point(41, 216);
             this.FNTxtBox.Name = "FNTxtBox";
             this.FNTxtBox.Size = new System.Drawing.Size(187, 24);
             this.FNTxtBox.TabIndex = 2;
@@ -65,25 +71,25 @@
             // MNTxtBox
             // 
             this.MNTxtBox.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.MNTxtBox.Location = new System.Drawing.Point(226, 118);
+            this.MNTxtBox.Location = new System.Drawing.Point(234, 216);
             this.MNTxtBox.Name = "MNTxtBox";
-            this.MNTxtBox.Size = new System.Drawing.Size(187, 24);
+            this.MNTxtBox.Size = new System.Drawing.Size(151, 24);
             this.MNTxtBox.TabIndex = 3;
             // 
             // LNTxtBox
             // 
             this.LNTxtBox.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.LNTxtBox.Location = new System.Drawing.Point(419, 118);
+            this.LNTxtBox.Location = new System.Drawing.Point(391, 216);
             this.LNTxtBox.Name = "LNTxtBox";
-            this.LNTxtBox.Size = new System.Drawing.Size(187, 24);
+            this.LNTxtBox.Size = new System.Drawing.Size(167, 24);
             this.LNTxtBox.TabIndex = 4;
             // 
             // SFTxtBox
             // 
             this.SFTxtBox.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.SFTxtBox.Location = new System.Drawing.Point(621, 118);
+            this.SFTxtBox.Location = new System.Drawing.Point(564, 216);
             this.SFTxtBox.Name = "SFTxtBox";
-            this.SFTxtBox.Size = new System.Drawing.Size(92, 24);
+            this.SFTxtBox.Size = new System.Drawing.Size(54, 24);
             this.SFTxtBox.TabIndex = 5;
             // 
             // NameLabl
@@ -92,11 +98,11 @@
             this.NameLabl.BackColor = System.Drawing.Color.Transparent;
             this.NameLabl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NameLabl.ForeColor = System.Drawing.Color.White;
-            this.NameLabl.Location = new System.Drawing.Point(39, 91);
+            this.NameLabl.Location = new System.Drawing.Point(39, 189);
             this.NameLabl.Name = "NameLabl";
-            this.NameLabl.Size = new System.Drawing.Size(63, 24);
+            this.NameLabl.Size = new System.Drawing.Size(99, 24);
             this.NameLabl.TabIndex = 6;
-            this.NameLabl.Text = "Name";
+            this.NameLabl.Text = "Full Name";
             // 
             // CTLabl
             // 
@@ -105,30 +111,21 @@
             this.CTLabl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.CTLabl.Font = new System.Drawing.Font("Alphakind", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CTLabl.ForeColor = System.Drawing.Color.Brown;
-            this.CTLabl.Location = new System.Drawing.Point(46, 33);
+            this.CTLabl.Location = new System.Drawing.Point(136, 111);
             this.CTLabl.Name = "CTLabl";
-            this.CTLabl.Size = new System.Drawing.Size(319, 44);
+            this.CTLabl.Size = new System.Drawing.Size(569, 44);
             this.CTLabl.TabIndex = 7;
-            this.CTLabl.Text = "CONTACT TRACING";
-            // 
-            // DtPckr
-            // 
-            this.DtPckr.CalendarFont = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DtPckr.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DtPckr.Location = new System.Drawing.Point(553, 33);
-            this.DtPckr.Name = "DtPckr";
-            this.DtPckr.Size = new System.Drawing.Size(263, 27);
-            this.DtPckr.TabIndex = 8;
+            this.CTLabl.Text = "VISITOR CONTRACT TRACING FORM";
             // 
             // FNLabl
             // 
             this.FNLabl.AutoSize = true;
             this.FNLabl.BackColor = System.Drawing.Color.Transparent;
-            this.FNLabl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FNLabl.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FNLabl.ForeColor = System.Drawing.Color.White;
-            this.FNLabl.Location = new System.Drawing.Point(72, 145);
+            this.FNLabl.Location = new System.Drawing.Point(80, 243);
             this.FNLabl.Name = "FNLabl";
-            this.FNLabl.Size = new System.Drawing.Size(107, 24);
+            this.FNLabl.Size = new System.Drawing.Size(89, 20);
             this.FNLabl.TabIndex = 9;
             this.FNLabl.Text = "First Name";
             // 
@@ -136,11 +133,11 @@
             // 
             this.MNLabl.AutoSize = true;
             this.MNLabl.BackColor = System.Drawing.Color.Transparent;
-            this.MNLabl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MNLabl.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MNLabl.ForeColor = System.Drawing.Color.White;
-            this.MNLabl.Location = new System.Drawing.Point(256, 145);
+            this.MNLabl.Location = new System.Drawing.Point(264, 243);
             this.MNLabl.Name = "MNLabl";
-            this.MNLabl.Size = new System.Drawing.Size(131, 24);
+            this.MNLabl.Size = new System.Drawing.Size(109, 20);
             this.MNLabl.TabIndex = 10;
             this.MNLabl.Text = "Middle Name";
             // 
@@ -148,11 +145,11 @@
             // 
             this.LNLabl.AutoSize = true;
             this.LNLabl.BackColor = System.Drawing.Color.Transparent;
-            this.LNLabl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNLabl.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LNLabl.ForeColor = System.Drawing.Color.White;
-            this.LNLabl.Location = new System.Drawing.Point(462, 145);
+            this.LNLabl.Location = new System.Drawing.Point(433, 243);
             this.LNLabl.Name = "LNLabl";
-            this.LNLabl.Size = new System.Drawing.Size(105, 24);
+            this.LNLabl.Size = new System.Drawing.Size(88, 20);
             this.LNLabl.TabIndex = 11;
             this.LNLabl.Text = "Last Name";
             // 
@@ -160,26 +157,100 @@
             // 
             this.SufLabl.AutoSize = true;
             this.SufLabl.BackColor = System.Drawing.Color.Transparent;
-            this.SufLabl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SufLabl.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SufLabl.ForeColor = System.Drawing.Color.White;
-            this.SufLabl.Location = new System.Drawing.Point(636, 145);
+            this.SufLabl.Location = new System.Drawing.Point(566, 243);
             this.SufLabl.Name = "SufLabl";
-            this.SufLabl.Size = new System.Drawing.Size(60, 24);
+            this.SufLabl.Size = new System.Drawing.Size(52, 20);
             this.SufLabl.TabIndex = 12;
             this.SufLabl.Text = "Suffix";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.textBox1.Location = new System.Drawing.Point(660, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 24);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.textBox2.Location = new System.Drawing.Point(697, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(38, 24);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.textBox3.Location = new System.Drawing.Point(741, 229);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 24);
+            this.textBox3.TabIndex = 15;
+            // 
+            // DtVstLbl
+            // 
+            this.DtVstLbl.AutoSize = true;
+            this.DtVstLbl.BackColor = System.Drawing.Color.Transparent;
+            this.DtVstLbl.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DtVstLbl.ForeColor = System.Drawing.Color.White;
+            this.DtVstLbl.Location = new System.Drawing.Point(656, 189);
+            this.DtVstLbl.Name = "DtVstLbl";
+            this.DtVstLbl.Size = new System.Drawing.Size(125, 24);
+            this.DtVstLbl.TabIndex = 16;
+            this.DtVstLbl.Text = "Date of Visit:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Circular Std Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(656, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "MM  /DD  /YY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Circular Std Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(656, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Time of Visit:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.textBox4.Location = new System.Drawing.Point(660, 324);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 24);
+            this.textBox4.TabIndex = 19;
             // 
             // ContactTracing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(851, 615);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DtVstLbl);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.SufLabl);
             this.Controls.Add(this.LNLabl);
             this.Controls.Add(this.MNLabl);
             this.Controls.Add(this.FNLabl);
-            this.Controls.Add(this.DtPckr);
             this.Controls.Add(this.CTLabl);
             this.Controls.Add(this.NameLabl);
             this.Controls.Add(this.SFTxtBox);
@@ -206,11 +277,17 @@
         private System.Windows.Forms.TextBox SFTxtBox;
         private System.Windows.Forms.Label NameLabl;
         private System.Windows.Forms.Label CTLabl;
-        private System.Windows.Forms.DateTimePicker DtPckr;
         private System.Windows.Forms.Label FNLabl;
         private System.Windows.Forms.Label MNLabl;
         private System.Windows.Forms.Label LNLabl;
         private System.Windows.Forms.Label SufLabl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label DtVstLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
